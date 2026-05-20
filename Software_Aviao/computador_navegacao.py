77175b7ce1b61b31fecb701ba5279a5597139fe2
Rafael Melo <rafael.gamelo@ufrpe.br>
2026-05-19 21:39:36 -0300

    Criação do Computador de Navegação

---
+++ Software_Aviao/computador_navegacao.py
@@ -0,0 +1,41 @@
+import json
+import time
+import random
+import paho.mqtt.client as mqtt
+
+BROKER = "broker.hivemq.com"
+PORTA = 1883
+TOPICO_NAV = "avionica/navegacao"
+
+def iniciar_navegacao():
+    cliente = mqtt.Client()
+    cliente.connect(BROKER, PORTA, 60)
+    print("🧭 Computador de Navegação e Automação INICIADO.")
+    print("A calcular rotas, proa e status do Piloto Automático...")
+    print("-" * 50)
+
+    waypoints = ["LISBOA_FIR", "WAYPOINT_ALFA", "ROTA_OCEANICA", "APROXIMACAO_FINAL"]
+
+    try:
+        while True:
+            # Simulando os cálculos de navegação
+            pacote = {
+                "origem": "Computador_Nav",
+                "dados": {
+                    "proa_graus": random.randint(265, 275), # Voando para Oeste
+                    "vs_fpm": random.randint(-150, 150),    # Razão de subida/descida (Pés por Minuto)
+                    "piloto_automatico": "LIGADO (LNAV/VNAV)",
+                    "waypoint_ativo": random.choice(waypoints),
+                    "eta_minutos": random.randint(15, 120)
+                }
+            }
+            cliente.publish(TOPICO_NAV, json.dumps(pacote))
+            print(f"📍 Navegação | Proa: {pacote['dados']['proa_graus']}° | Rumo a: {pacote['dados']['waypoint_ativo']}")
+            time.sleep(3) # Atualiza a cada 3 segundos
+            
+    except KeyboardInterrupt:
+        print("\nNavegação Desligada.")
+        cliente.disconnect()
+
+if __name__ == "__main__":
+    iniciar_navegacao()
