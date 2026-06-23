e9f10d2d4b1c4a4b5477adeef0cb2ac199bf68aa
JoaoLRS <lucas.jlrs252291@gmail.com>
2026-06-23 18:38:35 -0300

    Refatoração

--- backend/src/main/avionica/lamport/dto/TelemetriaPayloadDto.java
+++ backend/src/main/avionica/lamport/dto/TelemetriaPayloadDto.java
@@ -3,7 +3,7 @@ package avionica.lamport.dto;
 import lombok.Builder;
 
 @Builder
-public record TelemetriaPayload(
+public record TelemetriaPayloadDto(
     String origem,
     long logical_clock,
     String callsign,
